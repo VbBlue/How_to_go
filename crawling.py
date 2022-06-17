c920c66e8de281c28dc8225c8cfe7c8eecf4ffe7
VbBlue <quswjdtlr12@naver.com>
2022-06-17 16:20:09 +0900

    aa

--- crawling.py
+++ crawling.py
@@ -17,7 +17,7 @@ from datetime import datetime
 
 from GOHelp.models import Bizinfo
 
-@sched.scheduled_job('cron', hour='15', minute='0')
+@sched.scheduled_job('cron', hour='16', minute='3')
 @sched.scheduled_job('cron', hour='6')
 @sched.scheduled_job('cron', hour='18')
 def bizinfo_Crawaling():
