81ddcf03f22c77d13ba232c5e7cd16ff208531cd
VbBlue <quswjdtlr12@naver.com>
2022-06-18 16:25:53 +0900

    mobile update

---
+++ staticfiles/js/signup.js
@@ -0,0 +1,20 @@
+        document.querySelector('.img__btn').addEventListener('click', function() {
+            document.querySelector('.cont').classList.toggle('s--signup');
+        });
+
+    $(function() {
+        $("input").focus(function() {
+            $(this).parent().find("span").attr("style", "font-weight: bold; color: black;");
+        });
+        $("input").focusout(function() {
+            $(this).parent().find("span").attr("style", "");
+        });
+
+        $('html, body').animate({
+		    scrollTop: $('.cont').offset().top
+	    }, 0);
+	    if($(".form.sign-up").find(".forgot-pass").html().length > 0) {
+            $('.img__btn').trigger('click');
+	    }
+    });
+
